778f9d799f0872006ab07f09bbd260d0c0734b7b
Andrew4d3 <andrew4d3@gmail.com>
2019-11-02 14:45:39 -0300

    Lesson 2 - Assertion library example

--- Part-01/simple.js
+++ Part-01/simple.js
@@ -1,20 +1,25 @@
-// (1) Here we have two simple functions, one is adding and the other one substracting. But the Add one is wrong! It's easy to verify but let's write an automated test for doing so
-const sum = (a, b) => a - b;
-const substract = (a, b) => a - b;
+const { sum, subtract } = require("./math");
 
-// (2) We define the result and the expected value we know for sure that is correct
-let result = sum(2, 3);
-expected = 5;
-if (result !== expected) {
-	// (3) We verify if the result expected is correct
-	throw new Error(`${result} is not equal to ${expected}`); // (4) If it's not correct we throw an error
+let result, expected;
+
+result = sum(3, 7);
+expected = 10;
+expect(result).toBe(expected);
+
+result = subtract(7, 3);
+expected = 4;
+expect(result).toBe(expected);
+
+// (1) Instead of repeating if statements over and over, we can use this "expect" function that we can use anytime we want
+function expect(actual) {
+	return {
+		toBe(expected) {
+			// (2) Here we're applying the same logic than before
+			if (actual !== expected) {
+				throw new Error(`${actual} is not equal to ${expected}`);
+			}
+		}
+	};
 }
 
-// (4) The same for the other function
-result = substract(5, 2);
-expected = 3;
-if (result !== expected) {
-	throw new Error(`${result} is not equal to ${expected}`);
-}
-
-// (5) And that was one simple example on how to create an automated test
+// (3) That's how assertion libraries work!
