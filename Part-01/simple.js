ffb265cd78df6c7998251fce2c40707aef530207
Andrew4d3 <andrew4d3@gmail.com>
2019-11-02 19:09:23 -0300

    Lesson 3 - Building a testing framework

--- Part-01/simple.js
+++ Part-01/simple.js
@@ -1,25 +1,37 @@
+// (1) We need to find a way to encapsulate our tests, so that we can see the results of all of them
 const { sum, subtract } = require("./math");
 
-let result, expected;
+test("sum adds numbers", () => {
+	const result = sum(3, 7);
+	const expected = 10;
+	expect(result).toBe(expected);
+});
 
-result = sum(3, 7);
-expected = 10;
-expect(result).toBe(expected);
+test("subtract subtracts numbers", () => {
+	const result = subtract(7, 3);
+	const expected = 4;
+	expect(result).toBe(expected);
+});
 
-result = subtract(7, 3);
-expected = 4;
-expect(result).toBe(expected);
+// (2) We're going to use this tests function which will receive a title and a callback as parameters
+function test(title, callback) {
+	try {
+		// (3) If the callback runs without errors, the test passes
+		callback();
+		console.log(`✓ ${title}`);
+	} catch (error) {
+		// (4) Otherwise, it fails
+		console.error(`✕ ${title}`);
+		console.error(error);
+	}
+}
 
-// (1) Instead of repeating if statements over and over, we can use this "expect" function that we can use anytime we want
 function expect(actual) {
 	return {
 		toBe(expected) {
-			// (2) Here we're applying the same logic than before
 			if (actual !== expected) {
 				throw new Error(`${actual} is not equal to ${expected}`);
 			}
 		}
 	};
 }
-
-// (3) That's how assertion libraries work!
