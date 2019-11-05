cf07f47e523698247067621323475506b0bcf64e
Andrew Davis <reign-andrew@Andrews-MacBook-Pro.local>
2019-11-05 10:05:39 -0300

    Lesson 7 - Install, config and run eslint

---
+++ Part-02/src/example.js
@@ -0,0 +1,11 @@
+const foo = 'Freddy'
+typeof foo === 'string'
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser :-(
+}
+
+const greeting = 'hello'
+console.log(`${greeting} world!`)
+
+;[(1, 2, 3)].forEach(x => console.log(x))
