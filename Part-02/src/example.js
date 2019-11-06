c1de4d403d821558f120ffaad63d7c51b3b82a33
Andrew4d3 <andrew4d3@gmail.com>
2019-11-05 22:48:00 -0300

    Lesson 7 - Installing and Configuring Prettier

--- Part-02/src/example.js
+++ Part-02/src/example.js
@@ -1,11 +1,15 @@
-const foo = 'Freddy'
-typeof foo === 'string'
+// (1) Now we have prettier installed as dev-dependecy. Let's disorganize the formating here and then run:
+// $ npx prettier --write src/example.js
+// You will see how all the formating gets restored immediately
+const foo = "Freddy";
+typeof foo === "string";
 
-if (!('serviceWorker' in navigator)) {
+if (!("serviceWorker" in navigator)) {
   // you have an old browser :-(
 }
 
-const greeting = 'hello'
-console.log(`${greeting} world!`)
+const greeting = "hello";
+console.log(`${greeting} world!`);
+[(1, 2, 3)].forEach(x => console.log(x));
 
-;[(1, 2, 3)].forEach(x => console.log(x))
+// (2) But there's not need to use a command to run prettier. You can use through vscode. Just enable the extension and change to "format on save"
