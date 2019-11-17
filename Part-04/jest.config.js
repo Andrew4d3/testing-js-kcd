19f499ccd20242611964863fd7151e5c7790da2b
Andrew4d3 <andrew4d3@gmail.com>
2019-11-17 20:30:01 -0300

    Lesson 34 - Setting coverage Threshold in Jest

--- Part-04/jest.config.js
+++ Part-04/jest.config.js
@@ -2,11 +2,26 @@ const path = require('path')
 
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  // (6) By doing this, our jest environment will be able to understand any module from the src/shared folder as a regular node_module
   moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
   setupFilesAfterEnv: [require.resolve('./test/setup-tests.js')],
+  coverageThreshold: {
+    // (1) What if we want to set a specific coverage threshold for all the files? We can do it like this:
+    global: {
+      statements: 20,
+      branches: 6,
+      lines: 20,
+      functions: 20,
+    },
+    // (2) And what if we want to do different coverage setting for different files. As simple as this:
+    './Part-04/src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      lines: 100,
+      functions: 100,
+    },
+  },
 }
