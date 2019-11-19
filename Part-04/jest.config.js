57e3a3d8b89e1013ab8cff19f823df1a5080ec22
Andrew4d3 <andrew4d3@gmail.com>
2019-11-18 21:04:17 -0300

    Lesson 38 - Installing Jest plugins (typeahead)

--- Part-04/jest.config.js
+++ Part-04/jest.config.js
@@ -9,14 +9,12 @@ module.exports = {
   },
   setupFilesAfterEnv: [require.resolve('./test/setup-tests.js')],
   coverageThreshold: {
-    // (1) What if we want to set a specific coverage threshold for all the files? We can do it like this:
     global: {
       statements: 20,
       branches: 6,
       lines: 20,
       functions: 20,
     },
-    // (2) And what if we want to do different coverage setting for different files. As simple as this:
     './Part-04/src/shared/utils.js': {
       statements: 100,
       branches: 80,
@@ -24,4 +22,9 @@ module.exports = {
       functions: 100,
     },
   },
+  // (1) With jest we can use watch plugins, Like this one, which will ease the way we select test/files by typing a search pattern
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
